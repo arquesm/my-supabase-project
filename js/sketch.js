c152671300aa9a6253f1e4b6533c1751dcc4b300
magda <47281888+arquesm@users.noreply.github.com>
2025-11-30 22:18:44 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,5 @@
 // --- PUT YOUR SUPABASE KEYS HERE ---
-const SUPABASE_URL = 'https://mqleqkjgscpzkiyzbtxy.supabase.co/auth/v1/callback';
+const SUPABASE_URL = 'https://mqleqkjgscpzkiyzbtxy.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGVxa2pnc2NwemtpeXpidHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNDE5MzUsImV4cCI6MjA3OTgxNzkzNX0.dk5Pvf4RzpEHlM-8WiqmLAp74_6YhVLgWDp_GYN4F-g';
 
 // Initialize supabase client
