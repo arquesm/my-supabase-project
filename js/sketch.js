4d10a9e5b6060b88b53ed1414de1bd844e046a73
magda <47281888+arquesm@users.noreply.github.com>
2025-11-30 23:48:41 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -3,12 +3,12 @@ const SUPABASE_URL = 'https://mqleqkjgscpzkiyzbtxy.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGVxa2pnc2NwemtpeXpidHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNDE5MzUsImV4cCI6MjA3OTgxNzkzNX0.dk5Pvf4RzpEHlM-8WiqmLAp74_6YhVLgWDp_GYN4F-g';
 
 // Initialize supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
+const supabaseClient = supabaseClient.createClient(SUPABASE_URL, SUPABASE_ANON);
 
 let scoresData = []; // store fetched rows
 
 async function loadScores() {
-  const { data, error } = await supabase.from("scores").select("*");
+  const { data, error } = await supabaseClient.from("scores").select("*");
   if (error) {
     console.error("Supabase error:", error);
   } else {
@@ -19,7 +19,7 @@ async function loadScores() {
 loadScores();
 
 async function testSupabase() {
-  const { data, error } = await supabase.from("scores").select("*");
+  const { data, error } = await supabaseClient.from("scores").select("*");
   if (error) {
     console.error("Supabase error:", error);
   } else {
