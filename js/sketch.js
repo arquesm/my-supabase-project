1acd024e4e9088b1253a49da30b8a574096e4d44
magda <47281888+arquesm@users.noreply.github.com>
2025-11-30 23:14:04 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,8 @@
 // --- PUT YOUR SUPABASE KEYS HERE ---
-const SUPABASE_URL = 'https://mqleqkjgscpzkiyzbtxy.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGVxa2pnc2NwemtpeXpidHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNDE5MzUsImV4cCI6MjA3OTgxNzkzNX0.dk5Pvf4RzpEHlM-8WiqmLAp74_6YhVLgWDp_GYN4F-g';
+//const SUPABASE_URL = 'https://mqleqkjgscpzkiyzbtxy.supabase.co';
+//const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGVxa2pnc2NwemtpeXpidHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNDE5MzUsImV4cCI6MjA3OTgxNzkzNX0.dk5Pvf4RzpEHlM-8WiqmLAp74_6YhVLgWDp_GYN4F-g';
 
 // Initialize supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
+//const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
 
-async function testSupabase() {
-  const { data, error } = await supabase.from("scores").select("*");
-  console.log("Fetched rows:", data);
-  console.log("Supabase error:", error);
-}
 console.log("Hello from sketch.js!");
-
-testSupabase();
