6b4229524a2039838c9d98b641b47856069b3be7
magda <47281888+arquesm@users.noreply.github.com>
2025-11-30 23:52:14 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = 'https://mqleqkjgscpzkiyzbtxy.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGVxa2pnc2NwemtpeXpidHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNDE5MzUsImV4cCI6MjA3OTgxNzkzNX0.dk5Pvf4RzpEHlM-8WiqmLAp74_6YhVLgWDp_GYN4F-g';
 
 // Initialize supabase client
-const supabaseClient = supabaseClient.createClient(SUPABASE_URL, SUPABASE_ANON);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
 
 async function testSupabase() {
   const { data, error } = await supabaseClient.from("scores").select("*");
